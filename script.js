4967fa23bd7a7a82905c96132d941ebfa0a48c3b
na2co3 <na2co3.soda@gmail.com>
2016-08-20 00:13:36 +0900

    Improve tag insertion

--- script.js
+++ script.js
@@ -1,77 +1,85 @@
 
 var style_font_ext = document.createElement("style");
 style_font_ext.appendChild(document.createTextNode(
-	'@font-face{ font-family: "kardinal"; src: local("kardinal"), url("' + chrome.extension.getURL('kardinal.woff') + '") format("woff"); } @font-face{ font-family: "farf"; src: local("farf"), url("' + chrome.extension.getURL('farf.woff') + '") format("woff"); } .-cla-styles-rx-font-family { font-family: "kardinal", "farf" !important; } .-cla-styles-rx-invisible-marker { opacity: 0; white-space: pre; letter-spacing: -0.4em; }'));
+	'@font-face{ font-family: "kardinal"; src: local("kardinal"), url("' + chrome.extension.getURL('kardinal.woff') + '") format("woff"); } @font-face{ font-family: "farf"; src: local("farf"), url("' + chrome.extension.getURL('farf.woff') + '") format("woff"); } .-cla-styles-rx-font-family { font-family: "kardinal", "farf" !important; } .-cla-styles-rx-invisible-marker { font-size: 0; line-height: 0; }'));
 document.head.appendChild(style_font_ext);
 
 var insert_tags = function (muts)
 {
-
-	// for twitter, some confusing lines needed,
-	// because simple replacements make the links in the tweets disabled
-	if(window.location.href.match(/^https:\/\/twitter\.com\/.*/))
-	{
-		Array.prototype.forEach.call(document.getElementsByTagName('p'), function (p)
-		{
-			Array.prototype.forEach.call(p.childNodes, function (n)
-			{
-				// only replace in text nodes
-				if(n.nodeName == '#text')
-				{
-					if(n.nodeValue.match(/𐍂𐍇𐍊/))
-					{
-						var nodes = [];
-						Array.prototype.forEach.call(n.nodeValue.split(/\s?𐍂𐍇𐍊/), function (nv)
-						{
-							if(nv.match(/𐍊𐍂𐍇/))
-							{
-								var left = nv.split(/𐍊𐍂𐍇\s?/);
-								nodes.push(document.createTextNode(left[0]));
-								var e = document.createElement('span');
-								e.classList.add('-cla-styles-rx-font-family');
-								e.appendChild(document.createTextNode(left[1]));
-								nodes.push(e);
-							}
-							else nodes.push(document.createTextNode(nv));
-						});
-						nodes.forEach(function (node, i)
-						{
-							if(0==i) p.replaceChild(node, n);
-							else p.insertBefore(nodes[nodes.length-i], nodes[0].nextSibling);
-						});
-					}
-				}
-			});
-		});
-	}
-
 	// for tumblr, do not replace in the editor
-	else if(window.location.href.match(/^https?:\/\/[a-zA-Z0-9\-]+\.tumblr\.com\/.*/))
+	if(window.location.href.match(/^https?:\/\/[a-zA-Z0-9\-]+\.tumblr\.com\/.*/))
 	{
-		Array.prototype.forEach.call(document.querySelectorAll('div:not(.editor) > p'), function (p)
-		{
-			p.innerHTML = p.innerHTML.replace(/𐍊𐍂𐍇\s?/g, '<span class="-cla-styles-rx-font-family">').replace(/\s?𐍂𐍇𐍊/g, '</span>')
-		});
+		Array.prototype.forEach.call(document.querySelectorAll('div:not(.editor) > p'), replaceInNode);
 	}
 
-	// for other sites, simply replace the markers,
-	// but in order to deal with inputs in a form,
-	// it might be better to leave the markers, turning them invisible
 	else
 	{
-		Array.prototype.forEach.call(document.getElementsByTagName('p'), function (p)
-		{
-			p.innerHTML = p.innerHTML.replace(/𐍊𐍂𐍇\s?/g, '<span class="-cla-styles-rx-invisible-marker">𐍊𐍂𐍇</span><span class="-cla-styles-rx-font-family">').replace(/\s?𐍂𐍇𐍊/g, '</span><span class="-cla-styles-rx-invisible-marker">𐍂𐍇𐍊</span>')
-		});
-
+		Array.prototype.forEach.call(document.getElementsByTagName('p'), replaceInNode);
 	}
 };
+
+insert_tags();
 
 // support dynamic webpages
 var o = new MutationObserver(insert_tags);
 var conf =
 {
-	attributes: true,
+	childList:true,
 	subtree: true
 };
 o.observe(document.body, conf);
+
+function forEachTextNodes(parentNode, callback) {
+	if (!parentNode.hasChildNodes()) {
+		return;
+	}
+	if (parentNode.classList && parentNode.classList.contains("-cla-styles-rx-replaced")) {
+		return;
+	}
+	var childNodes = Array.prototype.slice.call(parentNode.childNodes);
+	childNodes.forEach(function (node) {
+		if (node.nodeType == Node.TEXT_NODE) {
+			callback(node, parentNode);
+		} else {
+			forEachTextNodes(node, callback);
+		}
+	});
+}
+
+function replaceInNode(p) {
+	forEachTextNodes(p, function(node, parentNode) {
+		if (node.nodeValue.match(/𐍊𐍂𐍇.*?𐍂𐍇𐍊/)) {
+			var newNodes = node.nodeValue.split(/(𐍊𐍂𐍇.*?𐍂𐍇𐍊)/g).map(function (text) {
+				var match, span, subSpan;
+				if ((match = text.match(/^𐍊𐍂𐍇\s?(.*?)\s?𐍂𐍇𐍊$/)) == null) {
+					return document.createTextNode(text);
+				} else {
+					span = document.createElement("span");
+					span.classList.add("-cla-styles-rx-replaced");
+
+					subSpan = document.createElement("span");
+					subSpan.classList.add("-cla-styles-rx-invisible-marker");
+					subSpan.appendChild(document.createTextNode("𐍊𐍂𐍇"));
+					span.appendChild(subSpan)
+
+					subSpan = document.createElement("span");
+					subSpan.classList.add("-cla-styles-rx-font-family");
+					subSpan.appendChild(document.createTextNode(match[1]));
+					span.appendChild(subSpan)
+
+					subSpan = document.createElement("span");
+					subSpan.classList.add("-cla-styles-rx-invisible-marker");
+					subSpan.appendChild(document.createTextNode("𐍂𐍇𐍊"));
+					span.appendChild(subSpan)
+
+					return span;
+				}
+			});
+			var nextNode = node.nextSibling
+			parentNode.removeChild(node);
+			newNodes.forEach(function(newNode) {
+				parentNode.insertBefore(newNode, nextNode);
+			});
+		}
+	});
+}
